ed3d8d2ff0aed152f16a2bbbefbd0e579df8c507
handbrennan <handbd@mail.uc.edu>
2022-02-23 18:36:44 -0500

    did initial parse stuff

--- app/src/main/java/com/ucmobiledevelopment/freeways/service/IncidentService.kt
+++ app/src/main/java/com/ucmobiledevelopment/freeways/service/IncidentService.kt
@@ -1,12 +1,23 @@
 package com.ucmobiledevelopment.freeways.service
 
+import com.ucmobiledevelopment.freeways.RetrofitClientInstance
+import com.ucmobiledevelopment.freeways.dao.IIncidentDAO
 import com.ucmobiledevelopment.freeways.dto.Incident
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 
 class IncidentService {
 
-    fun fetchIncidents() : List<Incident>? {
+    suspend fun fetchIncidents() : List<Incident>? {
+        withContext(Dispatchers.IO){
+            val service = RetrofitClientInstance.retrofitInstance?.create(IIncidentDAO::class.java)
+            val incidents = async {service?.getAllIncidents()}
+            var result = incidents.await()?.awaitResponse()?.body()
+            return@withContext result
+        }
+
 
     }
-
 
 }
