2c1fe30448c82e481d5ffb73e9b984f805d2ea9a
handbrennan <handbd@mail.uc.edu>
2022-02-26 19:21:59 -0500

    Added incidentList to IncidentService.kt, changed caseId to int in Incident.kt, updated gradle.xml to work with Java 11.

--- app/src/main/java/com/ucmobiledevelopment/freeways/service/IncidentService.kt
+++ app/src/main/java/com/ucmobiledevelopment/freeways/service/IncidentService.kt
@@ -12,15 +12,42 @@ import retrofit2.awaitResponse
 
 class IncidentService {
 
-    suspend fun fetchIncidents(fromCaseYear: Int, toCaseYear: Int, state: Int, county: Int) : IncidentListDTO? {
-       return withContext(Dispatchers.IO){
-            val service = RetrofitClientInstance.retrofitInstance?.create(IIncidentDAO::class.java)
-            val incidentListDTO = async {service?.getAllIncidents(fromCaseYear, toCaseYear, state, county)}
-            var result = incidentListDTO.await()?.awaitResponse()?.body()
-            return@withContext result
-        }
 
 
+    suspend fun fetchIncidents(fromCaseYear: Int, toCaseYear: Int, state: Int, county: Int) : List<Incident>? {
+       return withContext(Dispatchers.IO) {
+
+           var incidentList: List<Incident> = ArrayList<Incident>()
+
+           val service = RetrofitClientInstance.retrofitInstance?.create(IIncidentDAO::class.java)
+           val incidentListDTO =
+               async { service?.getAllIncidents(fromCaseYear, toCaseYear, state, county) }
+           var result = incidentListDTO.await()?.awaitResponse()?.body()
+
+           if (result != null) {
+               if(result.Results[0] != null){
+                    result.Results[0].forEach{
+                        var newIncident: Incident = Incident()
+
+                        newIncident.cityName = it.CITYNAME
+                        newIncident.countyId = it.COUNTY.toInt()
+                        newIncident.countyName = it.COUNTYNAME
+                        newIncident.stateId = it.STATE.toInt()
+                        newIncident.stateName = it.STATENAME
+                        newIncident.latitude = it.LATITUDE
+                        newIncident.longitude = it.LONGITUD
+                        newIncident.caseId = it.ST_CASE.toInt()
+                        newIncident.way1 = it.TWAY_ID
+                        newIncident.way2 = it.TWAY_ID2
+                        newIncident.vehiclesInvolved = it.TOTALVEHICLES.toInt()
+
+                        incidentList.toMutableList().add(newIncident)
+
+                    }
+               }
+           }
+           return@withContext incidentList
+       }
     }
 
 }
