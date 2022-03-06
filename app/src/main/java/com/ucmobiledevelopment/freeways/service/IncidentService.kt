8bb64cf4e9375b63d0ddc9a8ab7a6456d2ec866c
Andrew Glanz <andrewvita0@gmail.com>
2022-03-06 18:46:22 -0500

    Changed val names in IncidentDTO.kt to abide by Kotlin naming conventions. Immutable data should all be uppercase with words separated by an underscore.

--- app/src/main/java/com/ucmobiledevelopment/freeways/service/IncidentService.kt
+++ app/src/main/java/com/ucmobiledevelopment/freeways/service/IncidentService.kt
@@ -34,11 +34,11 @@ class IncidentService : IIncidentService {
                     result.Results[0].forEach{
                         var newIncident: Incident = Incident()
 
-                        newIncident.cityName = it.CITYNAME ?: ""
+                        newIncident.cityName = it.CITY_NAME ?: ""
                         newIncident.countyId = if(it.COUNTY != null) it.COUNTY.toInt() else 0
-                        newIncident.countyName = it.COUNTYNAME ?: ""
+                        newIncident.countyName = it.COUNTY_NAME ?: ""
                         newIncident.stateId = if(it.STATE != null) it.STATE.toInt() else 0
-                        newIncident.stateName = it.STATENAME ?: ""
+                        newIncident.stateName = it.STATE_NAME ?: ""
                         newIncident.latitude = it.LATITUDE ?: ""
                         newIncident.longitude = it.LONGITUDE ?: ""
                         newIncident.caseId =  if(it.ST_CASE != null) it.ST_CASE.toInt() else 0
