4ed27c859e60cfbf852c9179b805d47b81719ef1
J-Macleod <jacob.macleod10@gmail.com>
2022-03-13 04:44:02 -0400

    Report Incident Activity Created with Text Inputs

---
+++ app/src/main/java/com/ucmobiledevelopment/freeways/ReportIncidentActivity.kt
@@ -0,0 +1,11 @@
+package com.ucmobiledevelopment.freeways
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportIncidentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_incident)
+    }
+}
