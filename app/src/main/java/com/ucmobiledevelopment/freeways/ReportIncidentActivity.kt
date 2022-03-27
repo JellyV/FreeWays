aff4eda29fac84a462439951bfa97c36b1d56da5
J-Macleod <jacob.macleod10@gmail.com>
2022-03-27 12:12:26 -0400

    Back Button Created For Second Activity

--- app/src/main/java/com/ucmobiledevelopment/freeways/ReportIncidentActivity.kt
+++ app/src/main/java/com/ucmobiledevelopment/freeways/ReportIncidentActivity.kt
@@ -1,13 +1,18 @@
 package com.ucmobiledevelopment.freeways
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.ucmobiledevelopment.freeways.ui.theme.FreeWaysTheme
+import com.ucmobiledevelopment.freeways.ui.theme.Purple500
 
 class ReportIncidentActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,15 @@ class ReportIncidentActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+                val context = LocalContext.current
+                ExtendedFloatingActionButton (
+                    text = {  Text(text = "Back") },
+                    onClick = { val intent = Intent(context, MainActivity::class.java)
+                                context.startActivity(intent)
+                              },
+                    icon = { Icon(Icons.Filled.ArrowBack,"@drawable/back_icon") },
+                    backgroundColor = Purple500
+                )
             }
         }
     }
