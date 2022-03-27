adac7bdba2b0b094c88f1224a452f3d5883e53f5
J-Macleod <jacob.macleod10@gmail.com>
2022-03-27 11:24:51 -0400

    Second Activity and Floating Action Button

--- app/src/main/java/com/ucmobiledevelopment/freeways/MainActivity.kt
+++ app/src/main/java/com/ucmobiledevelopment/freeways/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.ucmobiledevelopment.freeways
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ucmobiledevelopment.freeways.ui.theme.FreeWaysTheme
+import com.ucmobiledevelopment.freeways.ui.theme.Purple200
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +35,16 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+                val context = LocalContext.current
+                ExtendedFloatingActionButton (
+                    text = {  Text(text = "Report Incident") },
+                    onClick = { val intent = Intent(context, ReportIncidentActivity::class.java)
+                        intent.putExtra("text", "text")
+                        context.startActivity(intent)
+                    },
+                    icon = { Icon(Icons.Filled.Add,"@drawable/plus_icon") },
+                    backgroundColor = Purple200
+                )
                 //TO DO: var foo = incidents (mock data to test in debugger)
                 //TO DO: var i = 1 + 1
             }
